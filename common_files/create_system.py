d34cdc55ba6ef2c9d073e9197310a26d4fcdff45
mondrov <117201125+mondrov@users.noreply.github.com>
2023-07-05 15:18:54 -0700

    Update create_system.py

--- common_files/create_system.py
+++ common_files/create_system.py
@@ -1,30 +1,11 @@
 from openmm import app, XmlSerializer
-from openmm.unit import kelvin, bar, nanometer
-from openmmforcefields.generators import SystemGenerator
-from openff.toolkit.topology import Molecule
+from openmm.unit import kelvin
 
-# Load the PDB file
-pdb = app.PDBFile('chignolin.pdb')
-
-# Define the forcefield parameters
-forcefield_kwargs = {'constraints': app.HBonds,
-                     'removeCMMotion': False}
-
-# Create the GBSAOBCForce
-gb_force = openmm.GBSAOBCForce()
-gb_force.setNonbondedMethod(openmm.GBSAOBCForce.HCT)
-gb_force.setSolventDielectric(1.0)
-gb_force.setSoluteDielectric(1.0)
-
-# Create the SystemGenerator
-system_generator = SystemGenerator(forcefields=['amber14/protein.ff14SB.xml', 'amber14/tip3p.xml'],
-                                   forcefield_kwargs=forcefield_kwargs)
+# Load the Amber file
+prmtop = app.AmberPrmtopFile('chignolin.prmtop')
 
 # Create the OpenMM System
-system = system_generator.create_system(pdb.topology)
-
-# Add the GBSAOBCForce to the System
-system.addForce(gb_force)
+system = prmtop.createSystem(nonbondedMethod=app.NoCutoff, constraints=app.HBonds, implicitSolvent=app.HCT)
 
 # Serialize the System to XML
 omm_sys_serialized = XmlSerializer.serialize(system)
@@ -32,4 +13,3 @@ omm_sys_serialized = XmlSerializer.serialize(system)
 # Save the serialized System to a file
 with open('system.xml', 'wt') as f:
     f.write(omm_sys_serialized)
-
