1827c6acb7252d7eda4aac6098e321707f45ea1c
mondrov <117201125+mondrov@users.noreply.github.com>
2023-07-05 15:36:21 -0700

    Create generate_bstate.sh

---
+++ generate_bstate.sh
@@ -0,0 +1,28 @@
+from openmm import app, XmlSerializer
+from openmm.unit import kelvin
+import os
+
+# Load the Amber files
+prmtop = app.AmberPrmtopFile('chignolin.prmtop')
+
+# Create the OpenMM System
+system = prmtop.createSystem(nonbondedMethod=app.NoCutoff, constraints=app.HBonds, implicitSolvent=app.HCT)
+
+# Load the PDB file
+pdb = app.PDBFile('chignolin_b.pdb')
+
+# Set the positions from the PDB file
+positions = pdb.getPositions(asNumpy=True)
+
+# Create the OpenMM Simulation with the positions
+integrator = LangevinMiddleIntegrator(275*kelvin,5/picosecond,.002*picoseconds)
+simulation = Simulation(pdb.topology,system,integrator)
+simulation.context.setPositions(pdb.positions)
+
+# Serialize the System to XML
+omm_sys_serialized = XmlSerializer.serialize(system)
+
+# Save the serialized System to a file in the bstates directory
+with open('bstates/bstate.xml', 'w') as f:
+    f.write(omm_sys_serialized)
+
