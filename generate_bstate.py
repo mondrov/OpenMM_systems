fb1fd373d44f3ecca8dcb4a61f8791740d3f8090
mondrov <117201125+mondrov@users.noreply.github.com>
2023-07-06 11:35:01 -0700

    Update generate_bstate.py

--- generate_bstate.py
+++ generate_bstate.py
@@ -1,18 +1,20 @@
-from openmm import app, XmlSerializer
-from openmm.unit import kelvin
+from openmm import XmlSerializer, LangevinMiddleIntegrator
+from openmm.app import Simulation, PDBFile, AmberPrmtopFile
+from openmm.app import HBonds, NoCutoff, HCT
+from openmm.unit import kelvin, picosecond
 import os
 
 # Load the Amber files
-prmtop = app.AmberPrmtopFile('common_files/chignolin.prmtop')
+prmtop = AmberPrmtopFile('common_files/chignolin.prmtop')
 
 # Create the OpenMM System
-system = prmtop.createSystem(nonbondedMethod=app.NoCutoff, constraints=app.HBonds, implicitSolvent=app.HCT)
+system = prmtop.createSystem(nonbondedMethod=NoCutoff, constraints=HBonds, implicitSolvent=HCT)
 
 # Load the PDB file
-pdb = app.PDBFile('bstate_chignolin.pdb')
+pdb = PDBFile('bstate_chignolin.pdb')
 
 # Create the OpenMM Simulation with the positions
-integrator = LangevinMiddleIntegrator(275*kelvin,5/picosecond,.002*picoseconds)
+integrator = LangevinMiddleIntegrator(275*kelvin,5/picosecond,.002*picosecond)
 simulation = Simulation(pdb.topology,system,integrator)
 simulation.context.setPositions(pdb.positions)
 
@@ -22,4 +24,3 @@ omm_sys_serialized = XmlSerializer.serialize(system)
 # Save the serialized System to a file in the bstates directory
 with open('bstates/bstate.xml', 'w') as f:
     f.write(omm_sys_serialized)
-
