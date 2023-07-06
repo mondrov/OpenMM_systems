7828aec8e01925c69cb0d47745981220e39b5064
mondrov <117201125+mondrov@users.noreply.github.com>
2023-07-06 10:19:11 -0700

    Update protein_prod.py

--- common_files/protein_prod.py
+++ common_files/protein_prod.py
@@ -2,6 +2,7 @@ from openmm import (
     XmlSerializer,
     LangevinIntegrator,
     Platform,
+    LangevinMiddleIntegrator
 )
 from openmm.unit import kelvin, picosecond
 from openmm.app import (
@@ -25,7 +26,7 @@ simulation = Simulation(pdb.topology, system, integrator, platform)
 simulation.context.setPositions(pdb.positions)
 
 simulation.loadState('parent.xml')
-simulation.reporters.append(StateDataReporter('seg.nfo', 5000, step=True, potentialEnergy=True, kineticEnergy=True, temperature=True))
+simulation.reporters.append(StateDataReporter('seg.nfo', 10000, step=True, potentialEnergy=True, kineticEnergy=True, temperature=True))
 simulation.reporters.append(HDF5Reporter('seg.h5', 10000))
-simulation.step(50000)
+simulation.step(10000)
 simulation.saveState('seg.xml')
